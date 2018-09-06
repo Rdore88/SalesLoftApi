72a7f1828755862e2da0dc9037f65c0e53274442
Robert Dore <robertdore@Roberts-MacBook-Pro.local>
2018-09-05 22:43:40 -0400

    Refactor button to be on the list

--- client/src/containers/allPeopleList/ConnectedAllPersonList.js
+++ client/src/containers/allPeopleList/ConnectedAllPersonList.js
@@ -2,9 +2,44 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ListItem from '../../components/listItem/ListItem';
 import './allPersonList.css';
-import LetterFrequencyButton from '../../components/letterFrequencyButton/LetterFrequencyButton';
 
 class AllPersonList extends Component {
+  constructor(props){
+    super(props);
+      this.findLetterFrequencies = this.findLetterFrequencies.bind(this);
+      this.state = {
+        notWords: [".", "_"]
+      }
+  }
+
+  organizeEmail(email){
+    let splitEmail = email.split("@")
+    return splitEmail[0]
+  }
+
+  getAllEmails(){
+    let emailArray = [];
+    this.props.allPeople.forEach(person => {
+      emailArray.push(this.organizeEmail(person.email_address))
+    })
+    return emailArray
+  }
+
+  findLetterFrequencies(event){
+    let output = {}
+    let allCharacters = this.getAllEmails().join('')
+
+    for (var i = 0; i < allCharacters.length; i++) {
+      if (output.hasOwnProperty(allCharacters[i])) {
+        output[allCharacters[i]] += 1
+      } else {
+        this.state.notWords.includes(allCharacters[i]) ? null : output[allCharacters[i]] = 1
+      }
+    }
+    console.log(output);
+    return output;
+  }
+
   render() {
     if (this.props.allPeople.length > 0) {
       let listOfPeople = this.props.allPeople.map(person => {
@@ -15,7 +50,7 @@ class AllPersonList extends Component {
       return (
         <div>
           <h1 className="list-header">List of all People</h1>
-          <LetterFrequencyButton peopleArray={this.props.allPeople}/>
+          <button onClick={this.findLetterFrequencies}>Letter Frequencies</button>
           <div className="list">
             {listOfPeople}
           </div>
