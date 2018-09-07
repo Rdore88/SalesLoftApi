2d35d102a952d5d1f1f47e9b8310ca274caada78
Rdore88 <robertdore@Roberts-MacBook-Pro.local>
2018-09-07 09:54:15 -0400

    Have tests for loading screen and a good api call

--- cypress/integration/people_list.spec.js
+++ cypress/integration/people_list.spec.js
@@ -1,6 +1,22 @@
-describe('reaching the home page', () => {
-  it('has a heading', () => {
+describe('reaching the home page with no data', () => {
+  it('has a loading header', () => {
+    cy.server()
+    cy.route({
+      method: 'GET',
+      url: '/api/me.json',
+      response: []
+    })
     cy.visit('http://localhost:5000/')
     cy.contains('Loading please wait')
+  })
+})
+
+describe('reaching the home page with data', () => {
+  it('has a list of from the response', () => {
+    cy.visit('http://localhost:5000/')
+    cy.contains('Loading please wait')
+    cy.contains('List of all People')
+    cy.get('.list-items').first()
+      .contains('Name: Marisa Casper')
   })
 })
